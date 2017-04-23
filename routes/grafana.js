c402fffc40fcf1a268f0023b646f3f4bf77308b8
Pavel Slama <info@pavelslama.cz>
2017-04-23 15:38:29 +0200

    V4

--- routes/grafana.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (router) {
-  router.route('/').get(function (req, res, next) {
-    res.render('pages/grafana', {
-      page: 'grafana',
-      title: 'Grafana'
-    })
-  })
-}
--- routes/terminal.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (router) {
-  router.route('/').get(function (req, res, next) {
-    res.render('pages/terminal', {
-      page: 'terminal',
-      title: 'Terminal'
-    })
-  })
-}
